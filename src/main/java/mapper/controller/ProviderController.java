15f1f01404aac34c3ffdfde2d0a2f3b6ccd155ec
oceenachi <phillipfrancisca@gmail.com>
2020-04-23 14:28:20 +0100

    Add controller to load data

--- src/main/java/mapper/controller/ProviderController.java
+++ src/main/java/mapper/controller/ProviderController.java
@@ -1,4 +1,19 @@
 package mapper.controller;
 
+import mapper.dto.request.Provider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/")
 public class ProviderController {
+
+    @PostMapping("/provider")
+    public ResponseEntity<?> loadData(@RequestBody Provider provider){
+       return providerService.addDate(Provider);
+    }
 }
