57847f8f70e91d0b2d0dd93e6138e12a65f3c75d
oceenachi <phillipfrancisca@gmail.com>
2020-04-24 08:12:45 +0100

    Add controller to return data based on specifications

--- src/main/java/mapper/controller/ProviderController.java
+++ src/main/java/mapper/controller/ProviderController.java
@@ -1,13 +1,18 @@
 package mapper.controller;
 
 import mapper.dto.request.Provider;
+import mapper.dto.request.QueryFields;
+import mapper.dto.response.ResponseData;
+import mapper.model.ProviderFields;
 import mapper.service.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -25,4 +30,17 @@ public class ProviderController {
     public ResponseEntity<?> loadData(@RequestBody Provider provider){
        return providerService.addData(provider);
     }
+
+    @GetMapping("/filter/{providerId}")
+    private List<ProviderFields> filterData(@PathVariable("providerId") String providerId,
+                                            @RequestParam(required = false) String name,
+                                            @RequestParam(required = false) String age,
+                                            @RequestParam(required = false) String timestamp) {
+
+       return providerService.filterData(Long.parseLong(providerId), providerService.mapRequestParam(name, age, timestamp));
+
+
+    }
+
+
 }
