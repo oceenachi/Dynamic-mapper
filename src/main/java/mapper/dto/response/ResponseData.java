f51cf4e78dfb41e9aa3d21d43397bf9c68a781e5
oceenachi <phillipfrancisca@gmail.com>
2020-04-24 08:22:12 +0100

    Model response structure

---
+++ src/main/java/mapper/dto/response/ResponseData.java
@@ -0,0 +1,13 @@
+package mapper.dto.response;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class ResponseData {
+
+    private String name;
+
+    private int age;
+
+    private Timestamp timestamp;
+}
--- src/main/java/mapper/repository/ProviderRepository.java
+++ src/main/java/mapper/repository/ProviderRepository.java
@@ -2,8 +2,9 @@ package mapper.repository;
 
 import mapper.model.ProviderFields;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProviderRepository extends JpaRepository<ProviderFields, Long> {
+public interface ProviderRepository extends JpaRepository<ProviderFields, Long>, JpaSpecificationExecutor<ProviderFields> {
 }
